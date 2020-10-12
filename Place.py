20c65b0ba4348cb709cb33b34f9668c23165f15f
Plath <cooper.plath@my.jcu.edu.au>
2020-10-12 11:31:25 +1000

    Accept GitHub invitation for assessment files

--- Place.py
+++ Place.py
@@ -1,2 +1,12 @@
 class Place:
-    def __init__(self):
+    def __init__(self, name, country, priority, visited):
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.visited = visited
+
+    def is_visited(self):
+        return self.visited == "v"
+
+    def __str__(self):
+        return
