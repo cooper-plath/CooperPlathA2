7f38a261c7c5fe1da587a265803959b4f56786dd
Plath <cooper.plath@my.jcu.edu.au>
2020-10-12 12:15:23 +1000

    add methods to place class as to check if priority is considered important with an integer above 2

--- Place.py
+++ Place.py
@@ -8,5 +8,9 @@ class Place:
     def is_visited(self):
         return self.visited == "v"
 
+    def is_important(self):
+        if self.priority >= 2:
+            print(" is important")
+
     def __str__(self):
-        return
+        return "{}, {}, {}, {}".format(self.name, self.country, self.priority, self.visited)
--- test_place.py
+++ test_place.py
@@ -1,5 +1,5 @@
 """(Incomplete) Tests for Place class."""
-from place import Place
+from Place import Place
 
 
 def run_tests():
@@ -7,17 +7,18 @@ def run_tests():
 
     # Test empty place (defaults)
     print("Test empty place:")
-    default_place = Place()
+    default_place = Place("Townsville", "Australia", 12, "v")
     print(default_place)
-    assert default_place.name == ""
-    assert default_place.country == ""
-    assert default_place.priority == 0
-    assert not default_place.is_visited
+    assert default_place.name == "Townsville"
+    assert default_place.country == "Australia"
+    assert default_place.priority == 12
+    assert default_place.is_visited
 
     # Test initial-value place
     print("Test initial-value place:")
-    new_place = Place("Malagar", "Spain", 1, False)
+    new_place = Place("Malagar", "Spain", 3, False)
     # TODO: Write tests to show this initialisation works
+
 
     # TODO: Add more tests, as appropriate, for each method
 
