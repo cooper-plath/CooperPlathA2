341a1f3de45a600c8ed0c9a9c7bba76af1fdbbaf
Plath <cooper.plath@my.jcu.edu.au>
2020-10-14 19:55:32 +1000

    create basic box layout as seen in task sheet for kivy app in app.kv

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ GitHub URL: https://github.com/cooper-plath/CooperPlathA2
 # Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
+from kivy.lang import Builder
 
 
 class TravelTrackerApp(App):
-    """..."""
-    pass
-
+    def build(self):
+        """ Link app.kv file to main build"""
+        self.title = "CooperPlathA2"
+        self.root = Builder.load_file('app.kv')
+        return self.root
 
 if __name__ == '__main__':
     TravelTrackerApp().run()
